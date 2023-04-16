5b653012f56f5e539b120ed832dd112052382b98
peppone-choi <peppone.choi@gmail.com>
2023-04-17 02:27:16 +0900

    기본 기능 구현 완료!

---
+++ src/main/java/com/zb/zerobase_mission1/HistoryService.java
@@ -0,0 +1,186 @@
+package com.zb.zerobase_mission1;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HistoryService {
+    public void userHistoryUpdate(History history){
+        // DB 접속 (IP, Port, 계정, Passwort, 인스턴스)
+
+        String url = "jdbc:mariadb://localhost:3306/testdb1";
+        String dbUserID = "testuser3";
+        String dbPassword = "3c2s1q!@#$";
+
+        // 드라이버 로드
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        Connection connection = null;
+        PreparedStatement prepeaedStatement = null;
+        String sql = "";
+        ResultSet rs = null;
+
+
+        // 커넥션 객체 생성
+
+        try {
+            connection = DriverManager.getConnection(url, dbUserID, dbPassword);
+
+            // 스테이트먼트 객체 실행
+
+
+            // 쿼리 실행
+            sql = "insert into history (NOW_LNT, NOW_LAT, SEARCH_DAY) " +
+                    "VALUES (?, ?, ?);";
+
+            prepeaedStatement = connection.prepareStatement(sql);
+
+
+
+
+            prepeaedStatement.setString(1, history.getLnt());
+            prepeaedStatement.setString(2, history.getLat());
+            prepeaedStatement.setString(3, history.getLocalTime());
+
+            int affected = prepeaedStatement.executeUpdate();
+
+//            PreparedStatement preparedStatement = null; 주로 이거 사용
+//            CallableStatement callableStatement = null;
+
+            // 결과 수행
+            if (affected > 0){
+                System.out.println("저장 성공");
+            }else {
+                System.out.println("저장 실패");
+            }
+
+            // 객체 연결 해제
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if(rs != null && !rs.isClosed()){
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                if(!prepeaedStatement.isClosed()){
+                    prepeaedStatement.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                if(!connection.isClosed()){
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public List<History> userHistroyList(){
+        // DB 접속 (IP, Port, 계정, Passwort, 인스턴스)
+
+        List<History> historyList = new ArrayList<>();
+
+        String url = "jdbc:mariadb://localhost:3306/testdb1";
+        String dbUserID = "testuser3";
+        String dbPassword = "3c2s1q!@#$";
+
+        // 드라이버 로드
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection connection = null;
+        PreparedStatement prepeaedStatement = null;
+        String sql = "";
+        ResultSet rs = null;
+
+        // 커넥션 객체 생성
+
+        try {
+            connection = DriverManager.getConnection(url, dbUserID, dbPassword);
+
+            // 스테이트먼트 객체 실행
+
+
+            // 쿼리 실행
+            sql = "select * from history order by SEARCH_DAY desc limit 20";
+
+            prepeaedStatement = connection.prepareStatement(sql);
+
+            rs = prepeaedStatement.executeQuery();
+
+//            PreparedStatement preparedStatement = null; 주로 이거 사용
+//            CallableStatement callableStatement = null;
+
+            // 결과 수행
+
+            while (rs.next()){
+                String HISTORY_ID = rs.getString("HISTORY_ID");
+                String NOW_LNT = rs.getString("NOW_LNT");
+                String NOW_LAT = rs.getString("NOW_LAT");
+                String SEARCH_DAY = rs.getString("SEARCH_DAY");
+
+
+
+                History history = new History(HISTORY_ID, NOW_LNT, NOW_LAT, SEARCH_DAY);
+
+                historyList.add(history);
+
+            }
+
+            // 객체 연결 해제
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if(rs != null && !rs.isClosed()){
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                if(!prepeaedStatement.isClosed()){
+                    prepeaedStatement.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                if(!connection.isClosed()){
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return historyList;
+    }
+}
--- src/main/webapp/history.jsp
+++ src/main/webapp/history.jsp
@@ -1,3 +1,6 @@
+<%@ page import="com.zb.zerobase_mission1.HistoryService" %>
+<%@ page import="com.zb.zerobase_mission1.History" %>
+<%@ page import="java.util.List" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 
 <!DOCTYPE html>
@@ -6,7 +9,10 @@
     <link rel="stylesheet" href="mission1.css">
 </head>
 <p>
-
+<%
+HistoryService historyService = new HistoryService();
+List<History> userHistroyList = historyService.userHistroyList();
+%>
 <h1>위치 히스토리 목록</h1>
 <div style="padding: 2px 0px 5px 0px"><a href="index.jsp">홈</a> | <a href="history.jsp">위치 히스토리 목록</a> | <a href="load-wifi.jsp">OpenApi 와이파이 정보 가져오기</a> | <a href="#">북마크 보기</a> | <a href="#">북마크 그룹 관리</a> </div>
 <table id="wifi-table">
@@ -16,12 +22,19 @@
         <th style="text-align:center">조회일자</th>
         <th style="text-align:center">비고</th>
     </tr>
+<%
+    for (History history:userHistroyList) {
+%>
     <tr>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
+        <td><%=history.getLat()%></td>
+        <td><%=history.getLnt()%></td>
+        <td><%=history.getLocalTime()%></td>
+        <td><form><input type="button" value="삭제"></form></td>
     </tr>
+<%    }
+
+%>
+
 </table>
 
 </body>
